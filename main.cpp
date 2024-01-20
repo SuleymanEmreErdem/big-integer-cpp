696699c08c13972e35425173f143e1d81792268a
Süleyman Emre Erdem <121464020+SuleymanEmreErdem@users.noreply.github.com>
2024-01-20 18:09:04 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "big.cpp"
+using namespace std;
+
+int main(){
+    big a, b;
+
+    a = "15000000000000000000000000000000000000000000"; b = "500000000000000000000000000000000000000000000000000000000000";
+
+    cout << a + b;
+
+    return 0;
+}
